3187994b94fc4caba3e84f69fa1d6dec7903e63a
Tristan Jesse <tristanljesse@gmail.com>
2023-01-09 20:35:26 +0000

    UPDATE: add amptify logo, style changes to blog

--- src/app/views/blog/pages/blog/blog.component.ts
+++ src/app/views/blog/pages/blog/blog.component.ts
@@ -9,7 +9,9 @@ import {takeUntil} from 'rxjs/operators'
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  preserveWhitespaces: true,
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class BlogComponent implements OnInit {
   private _onDestroy = new Subject()
